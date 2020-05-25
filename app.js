9d82cd9efe105ce475c66ebfbfc182680b4c2e0d
sanuriyp <55776117+sanuriyp@users.noreply.github.com>
2020-05-25 10:00:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,27 @@ console.log(new tutorial.SomeMathObject());
 const EventEmitter = require('events');
 const eventEmitter =new EventEmitter();
 
-eventEmitter.on('tutorial',()=>{
-    console.log('tutorial event has occurred');
+eventEmitter.on('tutorial',(num1,num2)=>{
+    console.log(num1+num2);
 });
 
-eventEmitter.emit('tutorial');
+eventEmitter.emit('tutorial',1,2);
+
+//event emitter class
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name=name;
+    }
+    get name(){
+        return this._name;
+    }
+}
+
+let sanuri=new Person('sanuri');
+sanuri.on('name',()=>{
+    console.log('My name is '+sanuri.name);
+});
+
+//exicute the emitter class
+sanuri.emit('name');
